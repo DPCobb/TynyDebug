4881be4c1cc3d9720d7db274791133f15fedb67d
ssmith-wombatweb <ssmith@wombatweb.us>
2017-02-19 19:26:26 -0800

    Added unit test for the versioner.

--- test/__tynydebug.js
+++ test/__tynydebug.js
@@ -108,3 +108,35 @@ describe('TynyDebug Utility Test', () => {
     });
   });
 });
+
+const version = require('../tynydebug').updateVersion;
+
+describe('TynyVersion Utility Test', () => {
+  describe('Test Version Change Function', () => {
+    it('Update the major version.', () => {
+      const newVer = version('1.3.2', 'major');
+
+      expect(newVer).to.equal('2.3.2');
+    });
+    it('Update the minor version.', () => {
+      const newVer = version('1.3.2', 'minor');
+
+      expect(newVer).to.equal('1.4.2');
+    });
+    it('Update the patch version.', () => {
+      const newVer = version('1.3.2', 'patch');
+
+      expect(newVer).to.equal('1.3.3');
+    });
+    it('Bad release type entered.', () => {
+      const newVer = version('1.3.2', 'match');
+
+      expect(newVer).to.equal('Invalid release type.');
+    });
+    it('Bad version number entered.', () => {
+      const newVer = version('1.3.2.3', 'match');
+
+      expect(newVer).to.equal('Invalid version number.');
+    });
+  });
+});
