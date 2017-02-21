ae0368dd52353ab40c7acc50663f1fa8db5b41af
ssmith-wombatweb <ssmith@wombatweb.us>
2017-02-20 23:19:56 -0800

    Added unit tests for the versioner.

--- test/__tynydebug.js
+++ test/__tynydebug.js
@@ -1,3 +1,4 @@
+const expect = require('chai').expect;
 const debug = require('../tynydebug.js');
 
 describe('TynyDebug Utility Test', () => {
@@ -32,5 +33,37 @@ describe('TynyDebug Utility Test', () => {
   });
   it('Tests the msg methods', () => {
     debug.msg('This is testing msg with no location');
+  });
+});
+
+const version = require('../tynydebug').updateVersion;
+
+describe('TynyVersion Utility Test', () => {
+  describe('Test Version Change Function', () => {
+    it('Update the major version.', () => {
+      const newVer = version('1.3.2', 'major');
+
+      expect(newVer).to.equal('2.3.2');
+    });
+    it('Update the minor version.', () => {
+      const newVer = version('1.3.2', 'minor');
+
+      expect(newVer).to.equal('1.4.2');
+    });
+    it('Update the patch version.', () => {
+      const newVer = version('1.3.2', 'patch');
+
+      expect(newVer).to.equal('1.3.3');
+    });
+    it('Bad release type entered.', () => {
+      const newVer = version('1.3.2', 'match');
+
+      expect(newVer).to.equal('Invalid release type.');
+    });
+    it('Bad version number entered.', () => {
+      const newVer = version('1.3.2.3', 'match');
+
+      expect(newVer).to.equal('Invalid version number.');
+    });
   });
 });
