98a7bbd4a8efb00f3f74fe4e8604dd6ae1f7a006
ssmith-wombatweb <ssmith@wombatweb.us>
2017-02-20 23:23:27 -0800

    Added commments.

--- tynydebug.js
+++ tynydebug.js
@@ -170,26 +170,26 @@ module.exports = {
       });
     }
   },
-  updateVersion(current, release) {
-    const v = current.split('.');
-    let num;
+  updateVersion(current, release) { // A function to semantically update the version number.
+    const v = current.split('.'); // Split the version number apart at the .
+    let num; // Variable for use in incrementing.
 
-    if (v.length === 3) {
+    if (v.length === 3) { // If there are 3 numbers in the version.
       switch (release) {
-        case 'major':
-          num = parseInt(v[0], 10) + 1;
-          return `${num}.0.0`;
-        case 'minor':
-          num = parseInt(v[1], 10) + 1;
-          return `${v[0]}.${num}.0`;
-        case 'patch':
-          num = parseInt(v[2], 10) + 1;
-          return `${v[0]}.${v[1]}.${num}`;
+        case 'major': // If the release is major.
+          num = parseInt(v[0], 10) + 1; // Add 1 to the major release.
+          return `${num}.0.0`; // Return the major release number with the minor and patch reset.
+        case 'minor': // If the release is minor.
+          num = parseInt(v[1], 10) + 1; // Add 1 to the minor release.
+          return `${v[0]}.${num}.0`; // Return the minor release number with the patch reset.
+        case 'patch': // If the release is a patch.
+          num = parseInt(v[2], 10) + 1; // Add 1 to the patch release.
+          return `${v[0]}.${v[1]}.${num}`; // Return the updated version.
         default:
-          return 'Invalid release type.';
+          return 'Invalid release type.'; // If the release is not one of the above, return the error.
       }
     }
 
-    return 'Invalid version number.';
+    return 'Invalid version number.'; // If the version number does not contain precisely 3 numbers return the error.
   },
 };
